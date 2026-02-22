7185e0328e023327df01691a230bc1d098e9f87a
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-02-22 21:36:35 +0100

    feat: implement users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import type { JwtPayload } from 'src/common/types/jwt-payload.type';
+import { Role } from 'src/common/enums/role.enum';
+import { UsersService } from './users.service';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  getUser(@CurrentUser() user: JwtPayload) {
+    return user;
+  }
+
+  @Patch('me')
+  updateUser() {
+    return console.log('user updated');
+  }
+
+  @Get('all')
+  @Roles(Role.ADMIN)
+  getAllUsers() {
+    return this.usersService.findAllUsers();
+  }
+
+  @Delete(':userId')
+  @Roles(Role.ADMIN)
+  deleteUser(@Param('userId') userId: string) {
+    return this.usersService.deleteUser(userId);
+  }
+
+  @Get(':userId/profile')
+  @Public()
+  getPublicProfile(@Param('userId') userId: string) {
+    return console.log(userId);
+  }
+}
