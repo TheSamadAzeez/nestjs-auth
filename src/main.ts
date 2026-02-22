b12feb272f49e322328dccdeb0b6f865ddf6fe2a
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-02-22 21:49:55 +0100

    feat: enable cors, helmet and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, //TODO: use config service
+    credentials: true, // allows cookies cross-origin
+  });
+
+  app.use(helmet()); // sets secure HTTP headers
+  app.use(cookieParser()); // needed to read req.cookies
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
